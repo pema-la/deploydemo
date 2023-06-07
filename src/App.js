cdafef90c2fefe89e5bbc9aa54822dbeec63bf3f
pema-la <84731608+pema-la@users.noreply.github.com>
2023-06-07 10:10:42 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <label class="switch">
+    <input checked="checked" type="checkbox"/>
+    <div class="button">
+      <div class="light"></div>
+      <div class="dots"></div>
+      <div class="characters"></div>
+      <div class="shine"></div>
+      <div class="shadow"></div>
+    </div>
+  </label>
+  );
+}
+
+export default App;
